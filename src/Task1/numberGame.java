a4206409eb6f14490d19abf51f626ea352366a45
Devesh Rawat <143208051+devesh-rawat@users.noreply.github.com>
2024-07-30 21:39:04 +0530

    repository Publish

---
+++ src/Task1/numberGame.java
@@ -0,0 +1,21 @@
+package Task1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class numberGame {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Random ran=new Random();
+        boolean playAgain=true;
+        int roundsWon=0;
+
+        while(playAgain){
+            int randomNum=ran.nextInt(100)+1;
+            int maxAttempts=10;
+            int attempts=0;
+            boolean hasWon=false;
+            
+        }
+    }
+}
