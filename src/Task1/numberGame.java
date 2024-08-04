87711318b46ecf522071b2b1c16cacea4829a194
Devesh Rawat <143208051+devesh-rawat@users.noreply.github.com>
2024-08-04 08:09:39 +0530

    Number Game code done

--- src/Task1/numberGame.java
+++ src/Task1/numberGame.java
@@ -5,17 +5,50 @@ import java.util.Scanner;
 
 public class numberGame {
     public static void main(String[] args) {
+        System.out.println("\t\t\t\t\t\t\t\t---------------------------------------------------------------");
+        System.out.println("\t\t\t\t\t\t\t\tWelcome to Number Guessing Game.....");
+        System.out.println("\t\t\t\t\t\t\t\t---------------------------------------------------------------");
+        System.out.println("\t\t\t\t\t\t\t\tDeveloped by Devesh Rawat");
+        System.out.println("\t\t\t\t\t\t\t\t---------------------------------------------------------------");
         Scanner sc=new Scanner(System.in);
         Random ran=new Random();
-        boolean playAgain=true;
         int roundsWon=0;
-
-        while(playAgain){
-            int randomNum=ran.nextInt(100)+1;
-            int maxAttempts=10;
+        boolean playGame=true;
+        while (playGame){
             int attempts=0;
-            boolean hasWon=false;
-            
+            int maxAttempts=10;
+            boolean wonGame=false;
+            int numToGuess=ran.nextInt(100)+1;
+            System.out.println("Guess a Number between 1 and 100.You have 10 attempts.");
+            while (attempts++<maxAttempts){
+                System.out.println("Guess a Number : ");
+                int userGuess=sc.nextInt();
+                if(userGuess<numToGuess){
+                    System.out.println("Too Low!");
+                }else if(userGuess>numToGuess){
+                    System.out.println("Too High!");
+                }else{
+                    System.out.println("Congratulations...You have Guessed the correct Number.");
+                    roundsWon++;
+                    wonGame=true;
+                    break;
+                }
+            }
+            if (!wonGame) {
+                System.out.println("Sorry, you've used all your attempts. The number was: " + numToGuess);
+            }
+
+            System.out.println("You have won " + roundsWon + " rounds.");
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            sc.nextLine();
+            String response = sc.nextLine();
+            playGame = response.equalsIgnoreCase("yes");
         }
+        System.out.println("Thank you for playing! You won " + roundsWon + " rounds.");
+        sc.close();
+
     }
-}
+
+    }
+
